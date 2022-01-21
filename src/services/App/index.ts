f5ecd61990c80b9f510384174eb95e6c239adbd6
euclidesdry <euclidesdry@gmail.com>
2022-01-21 07:08:14 +0100

    Fighting with CORS Policies 🤜 🧯

--- src/services/App/index.ts
+++ src/services/App/index.ts
@@ -1,11 +1,13 @@
-import axios from '../services.config';
+import axios from "../services.config";
 
 export async function getDrinks() {
   try {
-    const response = await axios.get("/search.php?s=coffee");
-    console.log(response)
+    const response = await axios.get(
+      "/top-headlines?country=us&category=business"
+    );
+    console.log(response);
     return response.data.drinks;
   } catch (error) {
     console.log(error);
   }
-};
+}
--- src/services/services.config.ts
+++ src/services/services.config.ts
@@ -1,13 +1,15 @@
 import axios from "axios";
 
+const API_KEY = "cd0ca7d83764493a9ce3be70836f041c";
+
 const instance = axios.create({
-  baseURL: "https://www.thecocktaildb.com/api/json/v1/1/",
+  baseURL: "https://newsapi.org/v2",
   timeout: 2000,
   headers: {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "DELETE, POST, GET, OPTIONS",
+    "Access-Control-Allow-Origin": "https://cors-anywhere.herokuapp.com/",
     "Access-Control-Allow-Headers":
       "Content-Type, Authorization, X-Requested-With",
+    "X-Api-Key": API_KEY,
   },
 });
 
