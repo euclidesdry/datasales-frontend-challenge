90f62bb6b3a723d6f394fcfb8bd35ad4d3813419
euclidesdry <euclidesdry@gmail.com>
2022-01-22 06:50:07 +0100

    Adding DrinksTable Component 🖱️

--- src/routes/App/index.tsx
+++ src/routes/App/index.tsx
@@ -1,20 +1,36 @@
-import React from 'react';
-import { Box } from '@mui/material';
+import React from "react";
+import { Box } from "@mui/material";
 
-import { DrinkList } from '../../components/App/Lists/DrinkList';
+import { DrinkList } from "../../components/App/Lists/DrinkList";
+import { DrinksTable } from "../../components/App/Table/DrinksTable";
+import { Header } from "../../components/App/Header";
 
 export default function App() {
   return (
-    <Box sx={{
-      with: '100vh',
-      height: '100vh',
-      display: 'flex',
-      justifyContent: 'center',
-      flexDirection: 'column',
-      alignItems: 'center'
-    }}>
-      <h1>Your touch on the app page</h1>
-      <DrinkList />
+    <Box
+      sx={{
+        with: "100vh",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <Header />
+      <h1>Drinks Table</h1>
+
+      <Box
+        sx={{
+          with: "80%",
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <DrinksTable />
+      </Box>
     </Box>
   );
 }
